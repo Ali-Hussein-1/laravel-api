f4df78c5f4e596b252ec947b718fb74dab4ed030
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-30 23:48:16 +0300

    Committing and cleaning the code

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,16 +5,44 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
-{
+{ 
+    // Function for the first API
+    function api1($str){
+        $res = "";
+        // declaring a reference string
+        $str1 = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789';
+        // looping through the reference string
+        for ($i = 0;$i<strlen($str1);$i++){
+            // looping through the user input
+            for ($j = 0;$j<strlen($str);$j++){
+                if ($str[$j] == $str1[$i])
+                $res = $res . $str[$j];
+            }
+        }
+        $message = $res;
+        // returning the results
+        return response()->json([
+            'status' => "Success",
+            'message' => $message]);
+        
+        }
+
+
+
+    // Function for the second API
     function api2($num){
         $arr = [];
-        // $num = strval( $num );
+        // getting the length of the input
         $len = strlen($num);
-        
+        // changing the input into an integer
         $num = (int)$num;
-
+        // checking if input is negative
+        if ($num < 0){
+            $num = -$num;
+        }
+        // looping through the digit of input number
         for ($i=0; $i< $len ; $i++){
-
+            // 
             $res = $num %(10**($len-$i));
 
             $res = $res - $num %(10**($len-$i-1));
@@ -24,40 +52,16 @@ class TestController extends Controller
   
        $message = $arr;
         
-
+        // returning the results
         return response()->json([
             "status" => "Success",
             "message" => $message
         ]);
-}
-
-    function api4($str){
-        $arr = explode(" ",$str);
-
-        
-        if($arr[0] == "+"){
-            $res = (int)$arr[1] + (int)$arr[2];
-        }
-        if($arr[0] == "-"){
-            $res = (int)$arr[1] - (int)$arr[2];
-        }
-        if($arr[0] == "*"){
-            $res = (int)$arr[1] * (int)$arr[2];
-        }
-        if($arr[0] == "**"){
-            $res = (int)$arr[1] ** (int)$arr[2];
-        }
-        if($arr[0] == "%"){
-            $res = (int)$arr[1] % (int)$arr[2];
-        }
-
-        $message = $res;
-
-        return response()->json([
-            "status" => "Success",
-            "message" => $message]);
     }
 
+    
+
+    // Function for the third API
     function api3($str){
         $string  = "";
         for($i = 0 ; $i <strlen($str) ; $i++){
@@ -69,26 +73,45 @@ class TestController extends Controller
 
         $message = decbin($string);
 
+        // returning the results
         return response()->json([
             "status" => "Success",
             "message" => $message]);
     }
-    function api1($str){
-        $res = "";
-        $str1 = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789';
-        for ($i = 0;$i<strlen($str1);$i++){
-            for ($j = 0;$j<strlen($str);$j++){
-                if ($str[$j] == $str1[$i])
-                $res = $res . $str[$j];
-            }
+
+
+   // Function for the fourth API
+    function api4($str){
+        // exploding the input string 
+        $arr = explode(" ",$str);
+        // checking for the operator and performing operations
+        // Performing addition
+        if($arr[0] == "+"){
+            $res = (int)$arr[1] + (int)$arr[2];
         }
+        // Performing substraction
+        if($arr[0] == "-"){
+            $res = (int)$arr[1] - (int)$arr[2];
+        }
+        // Performing multiplication
+        if($arr[0] == "*"){
+            $res = (int)$arr[1] * (int)$arr[2];
+        }
+        // Performing power
+        if($arr[0] == "**"){
+            $res = (int)$arr[1] ** (int)$arr[2];
+        }
+        // Performing modulo
+        if($arr[0] == "%"){
+            $res = (int)$arr[1] % (int)$arr[2];
+        }
+
         $message = $res;
+
+        // returning the results
         return response()->json([
-            'status' => "Success",
-            'message' => $message]);
-        
-        }
-
+            "status" => "Success",
+            "message" => $message]);
     }
-
+}
 ?>
--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 
-Route::get("/api2/{num?}", [TestController::class, 'api2']);
+Route::get("/api1/{str?}", [TestController::class, 'api1']);
 
-Route::get("/api4/{num?}", [TestController::class, 'api4']);
+Route::get("/api2/{num?}", [TestController::class, 'api2']);
 
 Route::get("/api3/{str?}", [TestController::class, 'api3']);
 
-Route::get("/api1/{str?}", [TestController::class, 'api1']);
+Route::get("/api4/{str?}", [TestController::class, 'api4']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
