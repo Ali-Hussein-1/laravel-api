2eae8cf1525d9fe95db024f5ae7d9bc0d12af8a0
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-30 21:46:00 +0300

    Adding the second API

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    function api2($num){
+        $arr = [];
+        // $num = strval( $num );
+        $len = strlen($num);
+        
+        $num = (int)$num;
+
+        for ($i=0; $i< $len ; $i++){
+
+            $res = $num %(10**($len-$i));
+
+            $res = $res - $num %(10**($len-$i-1));
+
+            array_push($arr , $res); 
+        }
+  
+       $message = $arr;
+        
+
+        return response()->json([
+            "status" => "Success",
+            "message" => $message
+        ]);
 }
+}
+
+?>
