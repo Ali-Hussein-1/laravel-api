6c77c73adbd0e595a0a6fbfc26f2930564d8eaa9
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-30 21:47:05 +0300

    Adding a route for the second API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 
+Route::get("/api2/{num?}", [TestController::class, 'api2']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
